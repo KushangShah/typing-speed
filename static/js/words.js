50d204a6ac3f44856ff196535e4b1adebe0385db
Kushang <124147164+KushangShah@users.noreply.github.com>
2026-01-24 11:46:14 +0530

    create a words.js for string random text that will be shown to user for practicing typing.

---
+++ static/js/words.js
@@ -0,0 +1,15 @@
+// static/js/words.js
+
+const WORDS = [
+    "hello","world","python","flask","typing","speed","test","random","keyboard","code",
+    "javascript","practice","result","monitor","screen","display","window","system","logic",
+    "function","variable","object","string","number","feature","simple","complex","build","deploy", "function", "return", "async", "await", "const", "let", "var", "import", "export",
+    "getElementById", "addEventListener", "JSON.stringify", "console.log", "null", "undefined",
+    "true", "false", "if (condition)", "for (let i=0;)", "array.map", "filter", "reduce",
+    "class", "extends", "constructor", "super", "this.state", "arrow =>", "function()",
+    "python", "__init__", "self", "def", "print", "lambda", "list", "dict", "tuple",
+    "flask", "@app.route", "render_template", "request", "response", "sqlalchemy", "jsonify",
+    "docker", "kubernetes", "git", "commit", "push", "pull", "merge", "branch", "checkout",
+    "sudo", "apt-get", "npm install", "yarn", "pip", "requirements.txt", "package.json",
+    "404", "500", "200 OK", "localhost:3000", "127.0.0.1", "https://", "ssh-rsa"
+];
